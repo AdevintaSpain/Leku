92a8f3e16a483804e64abe7d0dfbe1b16dde9113
Bernat Borrás <bernatbor15@gmail.com>
2016-12-20 15:48:42 +0100

    Update sample APP with POIS

--- app/src/main/java/com/schibsted/mappicker/MainActivity.java
+++ app/src/main/java/com/schibsted/mappicker/MainActivity.java
@@ -2,16 +2,20 @@ package com.schibsted.mappicker;
 
 import android.content.Intent;
 import android.location.Address;
+import android.location.Location;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
+import com.schibstedspain.leku.LekuPoi;
 import com.schibstedspain.leku.LocationPicker;
 import com.schibstedspain.leku.LocationPickerActivity;
 import com.schibstedspain.leku.tracker.LocationPickerTracker;
 import com.schibstedspain.leku.tracker.TrackEvents;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -37,25 +41,67 @@ public class MainActivity extends AppCompatActivity {
         startActivityForResult(intent, 1);
       }
     });
+
+    View mapPoisButton = findViewById(R.id.map_button_with_pois);
+    mapPoisButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        Intent intent = new Intent(getApplicationContext(), LocationPickerActivity.class);
+        intent.putExtra(LocationPickerActivity.LATITUDE, 41.4036299);
+        intent.putExtra(LocationPickerActivity.LONGITUDE, 2.1743558);
+        List<LekuPoi> pois = getLekuPois();
+        intent.putExtra(LocationPickerActivity.POIS_LIST, new ArrayList<>(pois));
+        startActivityForResult(intent, 2);
+      }
+    });
+
     initializeLocationPickerTracker();
+  }
+
+  private List<LekuPoi> getLekuPois() {
+    List<LekuPoi> pois = new ArrayList<>();
+
+    Location locationPoi1 = new Location("leku");
+    locationPoi1.setLatitude(41.4036339);
+    locationPoi1.setLongitude(2.1721618);
+    LekuPoi poi1 = new LekuPoi("Los bellota", locationPoi1);
+    pois.add(poi1);
+
+    Location locationPoi2 = new Location("leku");
+    locationPoi2.setLatitude(41.4023265);
+    locationPoi2.setLongitude(2.1741417);
+    LekuPoi poi2 = new LekuPoi("Starbucks", locationPoi2);
+    poi2.setAddress("Plaça de la Sagrada Família, 19, 08013 Barcelona");
+    pois.add(poi2);
+
+    return pois;
   }
 
   @Override
   protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-    if (requestCode == 1 && resultCode == RESULT_OK) {
+    if (resultCode == RESULT_OK) {
       Log.d("RESULT****", "OK");
-      double latitude = data.getDoubleExtra(LocationPickerActivity.LATITUDE, 0);
-      Log.d("LATITUDE****", String.valueOf(latitude));
-      double longitude = data.getDoubleExtra(LocationPickerActivity.LONGITUDE, 0);
-      Log.d("LONGITUDE****", String.valueOf(longitude));
-      String address = data.getStringExtra(LocationPickerActivity.LOCATION_ADDRESS);
-      Log.d("ADDRESS****", String.valueOf(address));
-      String postalcode = data.getStringExtra(LocationPickerActivity.ZIPCODE);
-      Log.d("POSTALCODE****", String.valueOf(postalcode));
-      Bundle bundle = data.getBundleExtra(LocationPickerActivity.TRANSITION_BUNDLE);
-      Log.d("BUNDLE TEXT****", bundle.getString("test"));
-      Address fullAddress = data.getParcelableExtra(LocationPickerActivity.ADDRESS);
-      Log.d("FULL ADDRESS****", fullAddress.toString());
+      if (requestCode == 1) {
+        double latitude = data.getDoubleExtra(LocationPickerActivity.LATITUDE, 0);
+        Log.d("LATITUDE****", String.valueOf(latitude));
+        double longitude = data.getDoubleExtra(LocationPickerActivity.LONGITUDE, 0);
+        Log.d("LONGITUDE****", String.valueOf(longitude));
+        String address = data.getStringExtra(LocationPickerActivity.LOCATION_ADDRESS);
+        Log.d("ADDRESS****", String.valueOf(address));
+        String postalcode = data.getStringExtra(LocationPickerActivity.ZIPCODE);
+        Log.d("POSTALCODE****", String.valueOf(postalcode));
+        Bundle bundle = data.getBundleExtra(LocationPickerActivity.TRANSITION_BUNDLE);
+        Log.d("BUNDLE TEXT****", bundle.getString("test"));
+        Address fullAddress = data.getParcelableExtra(LocationPickerActivity.ADDRESS);
+        Log.d("FULL ADDRESS****", fullAddress.toString());
+      } else if (requestCode == 2) {
+        double latitude = data.getDoubleExtra(LocationPickerActivity.LATITUDE, 0);
+        Log.d("LATITUDE****", String.valueOf(latitude));
+        double longitude = data.getDoubleExtra(LocationPickerActivity.LONGITUDE, 0);
+        Log.d("LONGITUDE****", String.valueOf(longitude));
+        String address = data.getStringExtra(LocationPickerActivity.LOCATION_ADDRESS);
+        Log.d("ADDRESS****", String.valueOf(address));
+      }
     }
     if (resultCode == RESULT_CANCELED) {
       Log.d("RESULT****", "CANCELLED");
