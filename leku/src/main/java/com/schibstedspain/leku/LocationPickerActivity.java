62df9acb52eeac82c7b7344c412ed32c51c5ba87
HNO Mobile <heinis.mark@gmail.com>
2017-03-20 13:15:47 +0100

    Update LocationPickerActivity.java

--- leku/src/main/java/com/schibstedspain/leku/LocationPickerActivity.java
+++ leku/src/main/java/com/schibstedspain/leku/LocationPickerActivity.java
@@ -423,7 +423,7 @@ public class LocationPickerActivity extends AppCompatActivity
     if (poisList != null) {
       savedInstanceState.putParcelableArrayList(POIS_LIST, new ArrayList<>(poisList));
     }
-    savedInstanceState.putBoolean(enableSatelliteView, true);
+    savedInstanceState.putBoolean(ENABLE_SATELLITE_VIEW, true);
     super.onSaveInstanceState(savedInstanceState);
   }
 
