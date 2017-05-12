a6de83b51393d89ec398b7f09a53e6d3ca0fb628
Marc Serra <marc.serra@scmspain.com>
2017-05-12 10:48:41 +0200

    Remove unused code

--- app/src/main/java/com/schibsted/mappicker/MainActivity.java
+++ app/src/main/java/com/schibsted/mappicker/MainActivity.java
@@ -44,6 +44,8 @@ public class MainActivity extends AppCompatActivity {
             //.withSatelliteViewHidden()
             .build(getApplicationContext());
 
+        Intent intent = new LocationPickerActivity.Builder().build(getApplicationContext());
+
         //this is optional if you want to return RESULT_OK if you don't set the latitude/longitude and click back button
         locationPickerIntent.putExtra("test", "this is a test");
 
