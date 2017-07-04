4febeda8b22e8c4578a5fe939bf1bf51a43951de
Bernat Borrás-Paronella Petit <bernatbor15@gmail.com>
2017-07-04 09:49:49 +0200

    Fix merge

--- leku/src/main/java/com/schibstedspain/leku/LocationPickerActivity.java
+++ leku/src/main/java/com/schibstedspain/leku/LocationPickerActivity.java
@@ -12,6 +12,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextWatcher;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
