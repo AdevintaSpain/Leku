cde27e6cde6eadc6476abec35016e53f0c4af5c3
Ferran Pons <ferran.pons@scmspain.com>
2018-01-19 09:18:51 +0100

    Zipped request results

--- leku/src/main/java/com/schibstedspain/leku/geocoder/GeocoderPresenter.java
+++ leku/src/main/java/com/schibstedspain/leku/geocoder/GeocoderPresenter.java
@@ -2,6 +2,7 @@ package com.schibstedspain.leku.geocoder;
 
 import android.annotation.SuppressLint;
 import android.location.Address;
+import android.support.annotation.NonNull;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.schibstedspain.leku.geocoder.places.GooglePlacesDataSource;
@@ -74,9 +75,9 @@ public class GeocoderPresenter {
 
   public void getFromLocationName(String query, LatLng lowerLeft, LatLng upperRight) {
     view.willLoadLocation();
-    Disposable disposable = Observable.concat(
+    Disposable disposable = Observable.zip(
         geocoderRepository.getFromLocationName(query, lowerLeft, upperRight),
-        getPlacesFromLocationName(query, lowerLeft, upperRight))
+        getPlacesFromLocationName(query, lowerLeft, upperRight), this::getMergedList)
         .subscribeOn(Schedulers.io())
         .observeOn(scheduler)
         .retry(RETRY_COUNT)
@@ -97,9 +98,9 @@ public class GeocoderPresenter {
 
   public void getDebouncedFromLocationName(String query, LatLng lowerLeft, LatLng upperRight, int debounceTime) {
     view.willLoadLocation();
-    Disposable disposable = Observable.concat(
+    Disposable disposable = Observable.zip(
         geocoderRepository.getFromLocationName(query, lowerLeft, upperRight),
-        getPlacesFromLocationName(query, lowerLeft, upperRight))
+        getPlacesFromLocationName(query, lowerLeft, upperRight), this::getMergedList)
         .subscribeOn(Schedulers.io())
         .debounce(debounceTime, TimeUnit.MILLISECONDS, Schedulers.io())
         .observeOn(scheduler)
@@ -125,6 +126,14 @@ public class GeocoderPresenter {
   private Observable<List<Address>> getPlacesFromLocationName(String query, LatLng lowerLeft, LatLng upperRight) {
     return isGooglePlacesEnabled ? googlePlacesDataSource.getFromLocationName(query, new LatLngBounds(lowerLeft, upperRight))
         .flatMapIterable(addresses -> addresses).take(MAX_PLACES_RESULTS).toList().toObservable()
-        .onErrorReturnItem(new ArrayList<>()) : Observable.empty();
+        .onErrorReturnItem(new ArrayList<>()) : Observable.just(new ArrayList<>());
+  }
+
+  @NonNull
+  private List<Address> getMergedList(List<Address> geocoderList, List<Address> placesList) {
+    List<Address> mergedList = new ArrayList<>();
+    mergedList.addAll(geocoderList);
+    mergedList.addAll(placesList);
+    return mergedList;
   }
 }
