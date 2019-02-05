8a21dd5c720c204727f7784706ad34415b180245
Ferran Pons <ferranpons@gmail.com>
2019-02-05 07:22:52 +0100

    Upgraded Google Places to the new Places SDK (#219)
    * Upgraded Google Places to the new Places SDK
    
    * The correct multidex import
    
    * solve issue with lint dependency conflict with androidx

--- app/src/main/java/com/schibsted/mappicker/SampleApplication.kt
+++ app/src/main/java/com/schibsted/mappicker/SampleApplication.kt
@@ -1,5 +1,5 @@
 package com.schibsted.mappicker
 
-import android.app.Application
+import androidx.multidex.MultiDexApplication
 
-class SampleApplication : Application()
+class SampleApplication : MultiDexApplication()
