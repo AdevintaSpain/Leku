b0a19cf894241399d7a219c29d3dba977223f737
Ferran Pons <ferranpons@gmail.com>
2019-07-30 09:22:38 +0200

    lint issues

--- app/src/main/java/com/schibsted/mappicker/MainActivity.kt
+++ app/src/main/java/com/schibsted/mappicker/MainActivity.kt
@@ -106,6 +106,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
         if (resultCode == Activity.RESULT_OK && data != null) {
             Log.d("RESULT****", "OK")
             if (requestCode == 1) {
