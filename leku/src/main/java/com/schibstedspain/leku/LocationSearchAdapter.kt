edf46b8fc81f38669f3830d5f19aeea4362b1b49
Ferran Pons <ferranpons@gmail.com>
2020-05-22 11:12:27 +0200

    lint issues

--- leku/src/main/java/com/schibstedspain/leku/LocationSearchAdapter.kt
+++ leku/src/main/java/com/schibstedspain/leku/LocationSearchAdapter.kt
@@ -8,8 +8,7 @@ import androidx.recyclerview.widget.RecyclerView
 
 class LocationSearchAdapter(
         private val locations: MutableList<String>?,
-        private val clickListener: SearchItemClickListener) : RecyclerView.Adapter<LocationSearchAdapter.SearchViewHolder>()
-{
+        private val clickListener: SearchItemClickListener) : RecyclerView.Adapter<LocationSearchAdapter.SearchViewHolder>() {
     class SearchViewHolder(val textView: TextView, private val clickListener: SearchItemClickListener) : RecyclerView.ViewHolder(textView), View.OnClickListener {
         init {
             textView.setOnClickListener(this)
