08928654c04611d43e7e92e626e21b5cd45366ff
Ferran Pons <ferranpons@gmail.com>
2022-09-29 14:58:15 +0200

    [NO ISSUE] Solved issue where none of the location permissions where granted returned an error

--- leku/src/main/java/com/adevinta/leku/utils/ReactiveLocationProvider.kt
+++ leku/src/main/java/com/adevinta/leku/utils/ReactiveLocationProvider.kt
@@ -1,8 +1,11 @@
 package com.adevinta.leku.utils
 
+import android.Manifest
 import android.content.Context
+import android.content.pm.PackageManager
 import android.location.Location
 import androidx.annotation.RequiresPermission
+import androidx.core.content.ContextCompat
 import com.google.android.gms.location.LocationServices
 import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
@@ -15,16 +18,21 @@ class ReactiveLocationProvider(
         anyOf = ["android.permission.ACCESS_COARSE_LOCATION", "android.permission.ACCESS_FINE_LOCATION"]
     )
     suspend fun getLastKnownLocation(): Location? = suspendCoroutine { task ->
-        LocationServices.getFusedLocationProviderClient(context)
-            .lastLocation
-            .addOnCompleteListener { locTask ->
-                task.resume(locTask.result)
-            }
-            .addOnCanceledListener {
-                task.resume(null)
-            }
-            .addOnFailureListener {
-                task.resume(null)
-            }
+        if (ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
+            || ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            LocationServices.getFusedLocationProviderClient(context)
+                .lastLocation
+                .addOnCompleteListener { locTask ->
+                    task.resume(locTask.result)
+                }
+                .addOnCanceledListener {
+                    task.resume(null)
+                }
+                .addOnFailureListener {
+                    task.resume(null)
+                }
+        } else {
+            task.resume(null)
+        }
     }
 }
