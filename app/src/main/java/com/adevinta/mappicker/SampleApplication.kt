9a7f83f8fe468e442cdc5618f1714066e838af03
Ferran Pons <ferranpons@gmail.com>
2023-09-27 10:17:34 +0200

    [NO ISSUE] Solved Maps initialization problem (#379)
    * [NO ISSUE] Solved Maps initialization problem
    
    * removed unused import

--- app/src/main/java/com/adevinta/mappicker/SampleApplication.kt
+++ app/src/main/java/com/adevinta/mappicker/SampleApplication.kt
@@ -1,11 +1,5 @@
 package com.adevinta.mappicker
 
 import androidx.multidex.MultiDexApplication
-import com.google.android.gms.maps.MapsInitializer
 
-class SampleApplication : MultiDexApplication() {
-    override fun onCreate() {
-        super.onCreate()
-        MapsInitializer.initialize(this)
-    }
-}
+class SampleApplication : MultiDexApplication()
